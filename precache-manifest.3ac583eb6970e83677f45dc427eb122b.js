ab8b21f1f4e77e1a07968a09bbdf8ed37322eadc
Code Sandbox <hello@codesandbox.io>
2023-07-12 05:25:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3ac583eb6970e83677f45dc427eb122b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443fed0ad031985fb6ca38f6b2413344",
+    "url": "https://koustav2.github.io/csb-rssvsd/index.html"
+  },
+  {
+    "revision": "97311db1353689211445",
+    "url": "https://koustav2.github.io/csb-rssvsd/static/css/main.f1f1e0cc.chunk.css"
+  },
+  {
+    "revision": "f9de6fc32b39369a6427",
+    "url": "https://koustav2.github.io/csb-rssvsd/static/js/2.7cf31f7d.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "https://koustav2.github.io/csb-rssvsd/static/js/2.7cf31f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97311db1353689211445",
+    "url": "https://koustav2.github.io/csb-rssvsd/static/js/main.e871bd99.chunk.js"
+  },
+  {
+    "revision": "e8d89ddc961149b19bcb",
+    "url": "https://koustav2.github.io/csb-rssvsd/static/js/runtime-main.f70e9804.js"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "https://koustav2.github.io/csb-rssvsd/static/media/logo.be622899.svg"
+  }
+]);
